4c20de1136841a9edcb5af2a91195217d43ad686
Ivan Epou <ivanepou92@gmail.com>
2022-12-28 01:55:13 +0300

    Refractor to use promises

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,43 +1,47 @@
 import { Request, Response, NextFunction } from 'express';
 import sharp from 'sharp';
-import fs from 'fs';
+import fs, { promises as fsPromises } from 'fs';
 import path from 'path';
 
-// Define the middleware function
-const imageMiddleware = (req: Request, res: Response, next: NextFunction) => {
+const imageMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const width = req.query.width as unknown as number;
   const height = req.query.height as unknown as number;
   const filename = req.query.filename as string;
 
+  //original Image path
   const orImageFilePath: string = `${__dirname}/../images/full/${filename}.jpg`;
+  //Already resized image path
   const thumbImageFilePath: string = `${__dirname}/../images/thumb/${filename}_${width}x${height}.jpg`;
-  // Check if the requested image exists in the "images" folder
-  fs.access(thumbImageFilePath, fs.constants.F_OK, (err) => {
-    if (err) {
-      // The image does not exist, so check if the original image exists in the "images" folder
-      fs.access(orImageFilePath, fs.constants.F_OK, (error) => {
-        if (error) {
-          // The original image does not exist, so return an error to the client
-          res.status(404).send('Image not found');
-        } else {
-          // The original image exists, so resize it using the "sharp" library
-          resizeImage(filename, width, height, res);
-        }
-      });
-    } else {
-      // The image exists, so send it back to the client as a response
-      res.sendFile(path.resolve(thumbImageFilePath), (err) => {
-        if (err) {
-          throw new Error(
-            'Server cannot serve you at the moment try again later'
-          );
-        }
-      });
+
+  // Check if the requested image exists in the "images/thumb" folder
+  try {
+    await fsPromises.access(thumbImageFilePath, fs.constants.F_OK);
+    // The image exists, so send it back to the client as a response
+    res.sendFile(path.resolve(thumbImageFilePath), (err) => {
+      if (err) {
+        throw new Error(
+          'Server cannot serve you at the moment try again later'
+        );
+      }
+    });
+  } catch (err) {
+    // The image does not exist, so check if the original image exists in the "images/full"
+    try {
+      await fsPromises.access(orImageFilePath, fs.constants.F_OK);
+      // The original image exists, so resize it using the "sharp" library
+      await resizeImage(filename, width, height, res);
+    } catch (error) {
+      // The original image does not exist, so return an error to the client
+      res.status(404).send('Image not found');
     }
-  });
+  }
 };
 
-const resizeImage = (
+const resizeImage = async (
   filename: string,
   imageWidth: number,
   imageHeight: number,
@@ -46,24 +50,21 @@ const resizeImage = (
   const filePath: string = `${__dirname}/../images/full/${filename}.jpg`;
   const resizedFilePath: string = `${__dirname}/../images/thumb/${filename}_${imageWidth}x${imageHeight}.jpg`;
 
-  sharp(filePath)
-    .resize(imageWidth / 1, imageHeight / 1)
-    .jpeg()
-    .toBuffer()
-    .then((outputBuffer) => {
-      // Save the resized image to the "images" folder
-      fs.writeFile(resizedFilePath, outputBuffer, (writeError) => {
-        if (writeError) {
-          throw new Error('Image was not created');
-        }
-      });
-      // Set the content type and send the image back to the client as a response
-      res.setHeader('Content-Type', 'image/jpeg');
-      res.send(outputBuffer);
-    })
-    .catch((resizeErr) => {
-      if (resizeErr) throw new Error('Image was not resized');
-    });
+  //use sharp library to resize the image
+  try {
+    const outputBuffer = await sharp(filePath)
+      .resize(imageWidth / 1, imageHeight / 1)
+      .jpeg()
+      .toBuffer();
+    // Save the resized image to the "images/thumb" folder
+    await fsPromises.writeFile(resizedFilePath, outputBuffer);
+
+    // Set the content type and send the image back to the client as a response
+    res.setHeader('Content-Type', 'image/jpeg');
+    res.send(outputBuffer);
+  } catch (error) {
+    throw new Error('Image was not resized');
+  }
 };
 
 export default imageMiddleware;
--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,14 +1,13 @@
-import supertest  from "supertest";
-import app  from "../index";
+import supertest from 'supertest';
+import app from '../index';
 
 //write end point test with jasmine
 const request = supertest(app);
 
 describe('Test end point response', () => {
-    it('Gets the Api End point',
-        async () => {
-            const response = await request.get('/api/v1');
-            expect(response.status).toBe(200);
-            // done();
-        });
+  it('Gets the Api End point', async () => {
+    const response = await request.get('/api/v1');
+    expect(response.status).toBe(200);
+    // done();
+  });
 });
