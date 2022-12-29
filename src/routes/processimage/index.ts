d52a41ac930155e341bd64bda9614fd223783fce
Ivan Epou <ivanepou92@gmail.com>
2022-12-29 16:54:23 +0300

    Fix types

--- src/routes/processimage/index.ts
+++ src/routes/processimage/index.ts
@@ -2,8 +2,12 @@ import express from 'express';
 import imageMiddleware from '../../middleware';
 const processImage = express.Router();
 
-processImage.get('/', imageMiddleware, async (req, res) => {
-  res.send('Image retrieved successfully');
-});
+processImage.get(
+  '/',
+  imageMiddleware,
+  async (req: express.Request, res: express.Response): Promise<void> => {
+    res.send('Image retrieved successfully');
+  }
+);
 
 export default processImage;
