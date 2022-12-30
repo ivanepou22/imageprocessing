14b99b85e139424f0b9744f143434be566de344d
Ivan Epou <ivanepou92@gmail.com>
2022-12-30 21:32:21 +0300

    Redefine the port

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import express from 'express';
 import routes from './routes';
 
 const app = express();
-const port = process.env.SERVER_PORT || 3000;
-//routing
 
+//defining the port
+const port = 3000;
+
+//Routing
 app.use('/api/v1', routes);
 
 app.listen(port, () => {
